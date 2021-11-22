24d2476b7fa3856565a5b3666efa5142ad01b741
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2021-11-22 08:43:52 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,129 @@
+// Маска ввода
+$(() => {
+	$('input[type=tel]').inputmask('+7 (999) 999-99-99')
+
+
+	$('body').on('click', '.modal_link', function (e) {
+	    e.preventDefault()
+        let text  = $(this).text();
+        // $("#callback_modal .modal_title").text(text);
+	    $.fancybox.close(true)
+
+	    $.fancybox.open({
+	        src: $(this).data('content'),
+	        type: 'inline',
+	        touch: false
+	    })
+	})
+
+    // Слайдер Команда
+
+    $('.specialist_items').slick({
+		infinite: true,
+		dots: true,
+		slidesToShow: 4,
+		slidesToScroll: 2,
+		arrows: true,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+                    slidesToScroll: 3,
+			    }
+			},
+			{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+                slidesToScroll: 1,
+			    }
+			},
+			{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+                slidesToScroll: 1,
+			    }
+			}
+		]
+	});
+
+
+    // Слайдер Отзывы
+
+    $('.reviews_items').slick({
+		infinite: true,
+		dots: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 1,
+                    slidesToScroll: 1,
+			    }
+			},
+			{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+                slidesToScroll: 1,
+			    }
+			},
+			{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+                slidesToScroll: 1,
+			    }
+			}
+		]
+	});
+
+
+  	$("form").submit(function() {
+        // Получение ID формы
+        var formID = $(this).attr('id');
+        // Добавление решётки к имени ID
+        var formNm = document.getElementById(formID);
+        $.ajax({
+            type: "POST",
+            url: 'send.php',
+            data: new FormData(formNm),
+            processData: false,
+            contentType: false,
+            beforeSend: function() {
+                // Вывод текста в процессе отправки
+                //$(formNm).html('<p style="text-align:center">Отправка...</p>');
+            },
+            success: function(data) {
+                // Вывод текста результата отправки
+                //$(formNm).html('<p style="text-align:center">' + data + '</p>');
+                 $.fancybox.close(true)
+			    $.fancybox.open({
+			        src: "#callback__modal2",
+			        type: 'inline',
+			        touch: false
+			    })
+			    $(formNm).trigger('reset');
+
+            },
+            error: function(jqXHR, text, error) {
+                // Вывод текста ошибки отправки
+                //$(formNm).html(error);
+            }
+        });
+        return false;
+    });
+
+
+})
+
+
+function handleFiles(file) {
+    const fileList = file;
+    $(".upload-file__text").text(fileList[0]["name"]);
+}
